3f40ce56f34be7ee26e9a6c8525c53a9539aa1a9
kurimaru <kurikuribaseball530@gmail.com>
2022-02-05 15:25:25 +0900

    firstCommit

---
+++ database/migrations/2022_02_05_054340_create_members_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable(false)->unique();
+            $table->integer('back_number')->nullable(false)->unique();
+            $table->integer('position');
+            $table->string('dominant');
+            $table->string('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
